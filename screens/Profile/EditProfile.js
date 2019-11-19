ed45ad9a1d18b403d06c6cfa8a953baeaff7f776
LahiruPathum <lahirupathum1223@gmail.com>
2019-11-19 16:27:20 +0530

    add editable feilds to edit profile screen

--- screens/Profile/EditProfile.js
+++ screens/Profile/EditProfile.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import Colors from '../../constants/Colors';
-import { Button , Overlay} from 'react-native-elements';
+import { Button , Overlay, Input} from 'react-native-elements';
 
 export default class EditProfile extends Component {
 
@@ -12,14 +12,123 @@ export default class EditProfile extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      name:'Lahiru'
     };
+    
+  }
+  current_data = this.props.navigation.state.params
+
+  save(){
+    alert('hello')
   }
 
   render() {
     return (
-      <View>
-        <Text> EditProfile </Text>        
-      </View>
+      <ScrollView>
+        <View style={styles.saveContainer}>
+          <Text>Save Your Changes</Text>
+          <Button
+            title='Save'
+            type='outline'
+            raised={true}
+            buttonStyle={styles.saveButton}
+            onPress={()=>this.save()}
+          />
+        </View>
+        <View style={styles.editField}>
+          <Input
+            label='Name :'
+            value={this.current_data.name}
+            leftIcon={{ type: 'material', name: 'account-circle', containerStyle:{marginRight:10} }}
+          />
+        </View>
+        <View style={styles.editField}>
+          <Input
+            label='E-Mail :'
+            value={this.current_data.email}
+            leftIcon={{ type: 'material', name: 'email', containerStyle:{marginRight:10} }}
+          />
+        </View>
+        <View style={styles.editField}>
+          <Input
+            label='Hometown :'
+            value={this.current_data.hometown}
+            leftIcon={{ type: 'material', name: 'room', containerStyle:{marginRight:10} }}
+          />
+        </View>
+        <View style={styles.editField}>
+          <Input
+            label='From :'
+            value={this.current_data.from}
+            leftIcon={{ type: 'material', name: 'room', containerStyle:{marginRight:10} }}
+          />
+        </View>
+        <View style={styles.editField}>
+          <Input
+            label='Country :'
+            value={this.current_data.country}
+            leftIcon={{ type: 'material', name: 'room', containerStyle:{marginRight:10} }}
+          />
+        </View>
+        <View style={styles.editField}>
+          <Input
+            label='Interest :'
+            value={this.current_data.interest}
+            leftIcon={{ type: 'material', name: 'pool', containerStyle:{marginRight:10} }}
+          />
+        </View>
+        <View style={styles.editField}>
+          <Input
+            label='Worksin :'
+            value={this.current_data.worksin}
+            leftIcon={{ type: 'material', name: 'work', containerStyle:{marginRight:10} }}
+          />
+        </View>   
+        <View style={styles.editField}>
+          <Input
+            label='Telenumber :'
+            value={this.current_data.telenumber}
+            leftIcon={{ type: 'material', name: 'phone', containerStyle:{marginRight:10} }}
+          />
+        </View> 
+        <View style={styles.editField}>
+          <Input
+            label='Bio :'
+            value={this.current_data.bio}
+            leftIcon={{ type: 'material', name: 'assignment', containerStyle:{marginRight:10} }}
+          />
+        </View> 
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  saveContainer:{
+    flex:1,
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginBottom:50
+  },
+  saveButton:{
+    width:70,
+  },
+  editField:{
+    flex:1,
+    flexDirection:'column',
+    justifyContent:'center',
+    alignItems:'flex-start',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+  },
+  editLable:{
+    fontSize:17,
+    paddingLeft:10
+  },
+  editInput:{
+
+  },
+})
