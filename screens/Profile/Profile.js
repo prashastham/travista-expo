a1f6077de9d04eed85ed320e544349922cb40adf
LahiruPathum <lahirupathum1223@gmail.com>
2019-12-26 22:28:21 +0530

    remove conflicts

--- screens/Profile/Profile.js
+++ screens/Profile/Profile.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React,{Component  } from "react";
 import { View,Text,YellowBox,StyleSheet,Platform,Dimensions,ScrollView,Image,SafeAreaView,Alert,TouchableOpacity,PlatformOSType } from "react-native";
 import {Avatar,Icon,Overlay,Button} from 'react-native-elements';
@@ -11,37 +10,10 @@ import Storage from '../../local/Storage';
 import firebaseClient from '../../local/FirebaseClient';
 import Colors from '../../constants/Colors';
 import Post from '../../elements/Post';
-=======
-import React, { Component } from "react";
-import {
-  View,
-  Text,
-  YellowBox,
-  StyleSheet,
-  Platform,
-  Dimensions,
-  ScrollView,
-  Image,
-  SafeAreaView,
-  Alert,
-  TouchableOpacity,
-  ImageBackground
-} from "react-native";
-import { Avatar, Icon, Overlay } from "react-native-elements";
-import { LinearGradient } from "expo-linear-gradient";
-import * as ImagePicker from "expo-image-picker";
-import Constants from "expo-constants";
-import * as Permissions from "expo-permissions";
-import Storage from "../../local/Storage";
-import firebaseClient from "../../local/FirebaseClient";
-import Colors from "../../constants/Colors";
-import Post from "../../elements/Post";
->>>>>>> 99dfa07830d02f8bf51d1915f63315d5482ef443
 import HeaderIcon from "../../components/HeaderIcon";
 import * as Progress from "react-native-progress";
 
 export default class Profile extends Component {
-<<<<<<< HEAD
  
     static navigationOptions = {
         title: "Profile",
@@ -80,43 +52,6 @@ export default class Profile extends Component {
       dpupload:false
     }
     async componentDidMount(){
-=======
-  static navigationOptions = {
-    title: "Profile",
-    headerTintColor: Colors.stackHeaderTintColor,
-    headerLeft: <HeaderIcon />
-  };
-
-  constructor(props) {
-    super(props);
-
-    YellowBox.ignoreWarnings([
-      "Warning: componentWillMount is deprecated",
-      "Warning: componentWillReceiveProps is deprecated",
-      "Warning: Setting a timer for a long period of time"
-    ]);
-  }
-  state = {
-    id: "1234567",
-    editable: "true", //this for identify own account. not still use!
-    email: "lahirupathum1223@gmail.com",
-    name: "Lahiru Pathum",
-    hometown: "Yakkalamulla",
-    from: "Galle",
-    country: "Sri Lanaka",
-    interest: "Beach",
-    worksin: "Travista Group",
-    telenumber: "0776480429",
-    dpurl:
-      "https://vignette4.wikia.nocookie.net/animal-jam-clans-1/images/7/75/Facepalm-cat-300x300.jpg/revision/latest?cb=20151223193525",
-    bio: "Hey there, this is about you. Say something shortly",
-    visible: false,
-    isOverlayVisible: false,
-    progress: 0,
-    dpupload: false
-  };
-  async componentDidMount() {
->>>>>>> 99dfa07830d02f8bf51d1915f63315d5482ef443
     //   let email = await Storage.getItem("email");
     //   let name = await Storage.getItem("name");
     //   let id = await Storage.getItem("id");
@@ -182,12 +117,8 @@ export default class Profile extends Component {
         uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
           console.log("File available at", downloadURL);
           blob.close();
-<<<<<<< HEAD
           this.setState({dpurl:downloadURL,dpupload:false,progress:0});
           this.updateimageurl();
-=======
-          this.setState({ dpurl: downloadURL, dpupload: false });
->>>>>>> 99dfa07830d02f8bf51d1915f63315d5482ef443
         });
       }
     );
@@ -195,7 +126,6 @@ export default class Profile extends Component {
     // We're done with the blob, close and release it
   };
 
-<<<<<<< HEAD
     //-------------------------------------------------------------------------
   
   updateimageurl = () =>{
@@ -226,39 +156,6 @@ export default class Profile extends Component {
       }
     })
   }
-=======
-  // updateimageurl = () =>{
-  //   const url = 'https://ireshd-7df6e.firebaseapp.com/api/update/'
-  //   const updateData = {
-  //     update:'dpurl',
-  //     value:this.state.dpurl,
-  //     id:this.state.id
-  //   }
-
-  //   fetch(url,{
-  //     method:'POST',
-  //     headers: {
-  //       'Accept': 'application/json',
-  //        'Content-Type': 'application/json'
-  //     },
-  //     body:JSON.stringify(updateData)
-  //   })
-  //   .then((res => res.json()))
-  //   .then((res) =>{
-  //     if(res.error === 'false')
-  //     {
-  //       Storage.removeItem('dpurl');
-  //       Storage.setItem('dpurl',{value:res.dpurl, id:1});
-  //       this.setState({visible:false,dpurl:res.dpurl});
-  //     }
-  //     else{
-  //       this.setState({visible:false});
-  //       alert(res.msg)
-  //     }
-
-  //   })
-  // }
->>>>>>> 99dfa07830d02f8bf51d1915f63315d5482ef443
 
   getPermissionAsync = async () => {
     if (Constants.platform.ios) {
@@ -360,7 +257,6 @@ export default class Profile extends Component {
               </Text>
             )}
           </View>
-<<<<<<< HEAD
           {/* ------------------------------------------------------------------------------------------------------ */}
           <Modal
           isVisible={this.state.isModalVisible}
@@ -410,53 +306,24 @@ export default class Profile extends Component {
               color = '#4ac959'
               containerStyle = {styles.menuicon}
               onPress={() => this.props.navigation.navigate('CreatePost')}
-=======
-          <View style={styles.menufield}>
-            <Icon
-              raised
-              size={24}
-              name="people"
-              type="material"
-              color="#2f95dc"
-              containerStyle={styles.menuicon}
-              onPress={() => this.props.navigation.navigate("Friend")}
->>>>>>> 99dfa07830d02f8bf51d1915f63315d5482ef443
             />
             <Icon
               raised
-<<<<<<< HEAD
               size={24}
               name="account-circle"
               type="material"
               color="#2f95dc"
               containerStyle={styles.menuicon}
               onPress={() => this.props.navigation.navigate("EditProfile")}
-=======
-              size = {24}
-              name = 'account-circle'
-              type = 'material'
-              color = '#4ac959'
-              containerStyle = {styles.menuicon}
-              onPress={() => this.props.navigation.navigate('EditProfile',this.state)}
->>>>>>> 2cd294cbee556b4e49e4b29a184d8ec96d3b2172
             />
             <Icon
               raised
-<<<<<<< HEAD
               size = {24}
               name = 'more-horiz'
               type = 'material'
               color = '#4ac959'
               containerStyle = {styles.menuicon}
               onPress={() => {this.setState({isModalVisible:true})}}
-=======
-              size={24}
-              name="more-horiz"
-              type="material"
-              color="#2f95dc"
-              containerStyle={styles.menuicon}
-              onPress={() => Actions.changeemail()}
->>>>>>> 99dfa07830d02f8bf51d1915f63315d5482ef443
             />
           </View>
           <View style={styles.datacontainer}>
@@ -575,7 +442,6 @@ export default class Profile extends Component {
   }
 }
 
-<<<<<<< HEAD
   const styles = StyleSheet.create({
  
     MainContainer: {
@@ -766,166 +632,4 @@ export default class Profile extends Component {
       fontWeight:'300',
       marginLeft:10,
     },
-=======
-const styles = StyleSheet.create({
-  MainContainer: {
-    paddingTop: Platform.OS === "ios" ? 20 : 0,
-    backgroundColor: "#fff"
-  },
-  namefield: {
-    flexGrow: 1,
-    flexDirection: "column",
-    paddingHorizontal: 10,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "transparent",
-    width: "100%",
-    height: 100,
-    marginVertical: 10
-  },
-  progressBar: {
-    flexGrow: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    paddingVertical: 10
-  },
-  nametext: {
-    fontSize: 30,
-    fontWeight: "300",
-    color: "#000",
-    textAlign: "center"
-  },
-  datacontainer: {
-    flexGrow: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "space-evenly",
-    marginVertical: 10
-  },
-  datafield: {
-    flexGrow: 1,
-    flexDirection: "row",
-    marginHorizontal: 10,
-    justifyContent: "flex-start",
-    alignItems: "center",
-    backgroundColor: "#fff",
-    width: "100%",
-    height: 35
-  },
-  datalable: {
-    fontSize: 17,
-    color: "#000",
-    marginHorizontal: 10
-  },
-  datatext: {
-    fontSize: 17,
-    fontWeight: "bold",
-    color: "#000"
-  },
-  dataicon: {
-    marginHorizontal: 5
-  },
-  menufield: {
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    alignItems: "center",
-    width: "100%",
-    height: 80,
-    paddingHorizontal: 10,
-    backgroundColor: "transparent",
-    borderTopWidth: 0.5,
-    borderTopColor: "#af4" //2de527
-  },
-  menuicon: {
-    backgroundColor: "#ff0",
-    width: 53,
-    height: 53,
-    borderRadius: 100,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  linearGradient: {
-    flexGrow: 1,
-    width: "100%",
-    height: 250,
-    alignItems: "center",
-    justifyContent: "center",
-    borderBottomRightRadius: 200
-  },
-  bioBtn: {
-    width: 50,
-    height: 25,
-    borderWidth: 2,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 25,
-    borderColor: "#2de527",
-    marginVertical: 10
-  },
-  bioBtnText: {
-    fontSize: 14,
-    color: "#2de527",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  bioText: {
-    fontSize: 17,
-    width: "75%",
-    marginTop: 10,
-    textAlign: "center",
-    justifyContent: "center"
-  },
-  profilegallerycontainer: {
-    flexGrow: 1,
-    width: "100%",
-    height: 330,
-    paddingVertical: 5,
-    borderTopWidth: 0.7,
-    borderColor: "#af4",
-    backgroundColor: "transparent",
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    alignItems: "center"
-  },
-  subgallerycontainer: {
-    width: "48%",
-    height: "98%",
-    backgroundColor: "transparent",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "space-evenly"
-  },
-  galleryimage: {
-    flexGrow: 1,
-    width: "100%",
-    marginVertical: 2,
-    borderRadius: 15
-  },
-  galleryedit: {
-    flexGrow: 1,
-    width: "100%",
-    height: 50,
-    paddingBottom: 10,
-    backgroundColor: "transparent",
-    alignItems: "center",
-    justifyContent: "center",
-    borderBottomWidth: 0.6,
-    borderBottomColor: "#af4"
-  },
-  galleryeditbtn: {
-    width: "90%",
-    height: 40,
-    backgroundColor: "#0000ff33",
-    borderRadius: 10,
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  galleryeditbtntext: {
-    alignItems: "center",
-    justifyContent: "center",
-    color: "#00f",
-    fontSize: 14,
-    fontWeight: "bold"
-  }
->>>>>>> 99dfa07830d02f8bf51d1915f63315d5482ef443
 });
